5efd6d5b65c073b22837491a6dd9652e71f198c7
George Novachuk <giafilmsyt@gmail.com>
2022-04-03 14:06:16 +0400

    Move btime -> NaiveDateTime conversion into function

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,23 @@ macro_rules! unwrap_or_exit {
         match $a {
             Ok(val) => val,
             Err(err) => {
-                println!("Error: Please report this to maintainers of edik, {}", err);
+                println!("Error: Please report this to maintainers of edik\n{}", err);
                 std::process::exit(1);
             }
         }
     };
+}
+
+pub fn btime_to_naive_date(duration: std::time::Duration) -> chrono::NaiveDateTime {
+    let unix_epoch_time = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    chrono::NaiveDateTime::from_timestamp(
+        (unix_epoch_time - duration.as_secs()) as i64,
+        0u32
+    )
 }
 
 pub fn get_file_creation_date(f: &std::fs::DirEntry) -> Result<chrono::NaiveDateTime, Box<dyn Error>>
@@ -21,13 +33,7 @@ pub fn get_file_creation_date(f: &std::fs::DirEntry) -> Result<chrono::NaiveDate
         .unwrap()
         .elapsed()
         .unwrap();
-    let unix_epoch_time = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    let date_created = chrono::NaiveDateTime::from_timestamp(
-        (unix_epoch_time - time_stamp.as_secs()) as i64,
-        032
-    );
+
+    let date_created = btime_to_naive_date(time_stamp);
     Ok(date_created)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use edik::unwrap_or_exit;
 
+struct FileDate {
+    original_name: String,
+}
+
 fn main() {
     let dir_path = "photos/";
     let files = std::fs::read_dir(dir_path).unwrap_or_else(|err| {
@@ -8,6 +12,7 @@ fn main() {
     });
     let re_naive_date = unwrap_or_exit!(regex::Regex::new(r#"^[0-9]{4}-[0-9]{2}-[0-9]{2}"#));
     let re_correct_data_fmt = unwrap_or_exit!(regex::Regex::new(r#"^[1-9]{4}-[0-9]{2}-[0-9]2 #[1-9]+"#));
+    let mut count: u32 = 3u32;
 
     // jpeg, mov, mp3, png
     files.for_each(|f_res| {
@@ -29,7 +34,7 @@ fn main() {
         }) { return; }
 
         let date_created = unwrap_or_exit!(edik::get_file_creation_date(&f));
-        let date_string = match re_naive_date.find(&date_created.to_string()) {
+        let date_string: String = match re_naive_date.find(&date_created.to_string()) {
             Some(ma) => ma.as_str().to_owned(),
             None => {
                 println!("debug: `{}`", &date_created.to_string());
@@ -40,9 +45,11 @@ fn main() {
         let mut new_file_path = String::new();
         new_file_path.push_str(dir_path);
         new_file_path.push_str(&date_string);
+        new_file_path.push_str(" #");
+        new_file_path.push_str(&count.to_string());
         new_file_path.push_str(".png");
 
-
+        count += 1;
         // TODO: Check if file names match, and number them (temporarily) accordingly
         std::fs::rename(f.path(), &new_file_path);
 
